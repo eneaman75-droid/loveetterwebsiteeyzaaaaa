975511010c0052700868d005bdaede6bbd2fb6bf
eneaman75-droid <eneaman75@gmail.com>
2026-01-13 18:05:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -12,27 +12,27 @@ const CONFIG = {
   memorialDate: '2025-06-01T12:30:00',
   
   // Love letter content
-  letter: {
+ letter: {
     paragraph1: [
-      '',
-      '',
-      '',
-      '',
-      '',
-      ''
+      "I tried my best here, okay AHAHAH.",
+      "I made this website only for you to show",
+      "how much I love you and because",
+      "the first one wasn't great AHAHAH.",
+      "I am so incredibly grateful",
+      "for you being in my life."
     ],
     paragraph2: [
-      '',
-      '',
-      '',
-      '',
-      ''
+      "Every time I text with you, I have a smile",
+      "on my face, and I catch myself",
+      "smiling again because of you.",
+      "I really love that a lot. Thank you for",
+      "smiling even on the bad days."
     ],
     paragraph3: [
-      '',
-      '',
-      '',
-      ''
+      "I really, truly love you, Beyza.",
+      "Don’t forget—I really, really do.",
+      "Thank youuu, and sorry for the bad times.",
+      "Oh, and right, I HAVE VIDEOSSS!"
     ]
   },
   
